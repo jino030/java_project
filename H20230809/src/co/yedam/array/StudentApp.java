467e456c198dcc48d4754c314280c35b137c4809
jino <sok030614@gmail.com>
2023-08-09 19:29:12 +0900

    StudentApp 학생등록, 학생목록 수

--- H20230809/src/co/yedam/array/StudentApp.java
+++ H20230809/src/co/yedam/array/StudentApp.java
@@ -11,16 +11,16 @@ public class StudentApp {
 		
 		// 1. 등록 | 2. 조회 | 3. 목록 | 4. 종료
 		Scanner scan = new Scanner(System.in);
+		int idx = 0;
 		
-		
+		System.out.println("또잉");
 		while(true) {
-			System.out.println(" =============================================== ");
+			System.out.println(	"\n =============================================== ");
 			System.out.println("  1. 학생 등록 | 2. 성적 조회 | 3. 학생 목록 | 4. 종료  ");
 			System.out.println(" =============================================== ");
 			
 			System.out.print(" 입력> ");
 			int num = scan.nextInt();
-			int idx = 0;
 			
 			switch(num) {
 			case 1 :
@@ -28,7 +28,7 @@ public class StudentApp {
 				Student st = new Student();
 				
 				System.out.print(" 이름 입력> ");
-				st.name = scan.nextLine();
+				st.name = scan.next();
 				
 				System.out.print(" 점수 입력> ");
 				st.score = scan.nextInt();
@@ -36,7 +36,7 @@ public class StudentApp {
 				students[idx] = st;
 				idx++;
 				
-				break;
+				continue;
 				
 			case 2 :
 				// 이름을 입력하면 점수를 출력
@@ -52,13 +52,17 @@ public class StudentApp {
 				
 			case 3 :
 				// 전체 목록 출력
-				System.out.println("[이 름] [점 수]");
+				System.out.println("");
+				System.out.println("   =====================");
+				System.out.println("     학생명   |    점수    ");
+				System.out.println("   =====================");
 				
-				for(int i=0; i<students.length; i++) {
-					System.out.printf("[%s] [%d점]", students[i].name, students[i].score);
+				for(int i=0; i<idx; i++) {
+					System.out.printf("     %s   |    %d점     \n", students[i].name, students[i].score);
+					System.out.println("   ---------------------");
 				}
 				
-				break;
+				continue;
 			
 			case 4 :
 				// 종료
